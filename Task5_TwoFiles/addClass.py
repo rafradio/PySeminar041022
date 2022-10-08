8fde964cc5c0bd5e890ddd687437b43383e93ae2
Rafail Abdyushev <rafaelab@mail.ru>
2022-10-08 16:34:06 +0300

    Task 5

---
+++ Task5_TwoFiles/addClass.py
@@ -0,0 +1,70 @@
+from operator import concat
+import os
+import random
+
+class InitSets:
+    def __init__(this):
+        clear = lambda: os.system("CLS")
+        clear()
+
+class ExecModule:
+    def __init__(this):
+        this.koeff = []
+        this.myFilestxt = ["myfile.txt", "myfile1.txt"]
+        this.FindFiles()
+        this.FindSum()
+        print(this.koeff)
+        print("Сумма равна: ", this.sum)
+
+    def FindFiles(this):
+        for i in range(2):
+            this.ReadFiles(this.myFilestxt[i])
+            this.AnalyseStrings()
+
+    def ReadFiles(this, filename):
+        f = open(filename, "r")
+        this.strFile1 = f.read()
+        f.close()
+        this.strFile1 = this.strFile1.replace(" ", "")
+        print(this.strFile1)
+
+    def AnalyseStrings(this):
+        posInt = ""
+        prev = ""
+        for i in this.strFile1:
+            try:
+                num = int(i)
+                checkFlag = True
+            except:
+                checkFlag = False
+
+            if checkFlag: 
+                posInt += str(num)
+            else:
+                if len(posInt) != 0:
+                    if i == "*" or i == "=":
+                        this.koeff.append(posInt)
+                posInt = ""
+                if i == "x":
+                    if prev == "" or prev =="+":
+                        this.koeff.append("1")
+
+            prev = i
+
+    def FindSum(this):
+        this.sum = 0
+        for i in this.koeff:
+            this.sum += int(i)
+
+        
+            
+        # if isinstance(num, int):
+        #     print("hello raf")
+        #     x = this.strFile1[0]
+        #     print(this.strFile1[0])
+        # else: print("no")
+        # for i in this.strFile1:
+        #     print()
+
+
+
